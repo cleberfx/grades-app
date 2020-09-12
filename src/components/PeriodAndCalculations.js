20ec2f5d150f6f7fc8c8b89e251b51b67a11c63b
cleberfx <cleberfx@yahoo.com.br>
2020-09-12 12:00:24 +0000

    useeffect

--- src/components/PeriodAndCalculations.js
+++ src/components/PeriodAndCalculations.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import service from '../services/TransactionService';
 // import Select from 'react-select';
 
